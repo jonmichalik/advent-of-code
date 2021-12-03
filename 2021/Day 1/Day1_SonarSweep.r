8d407fa713d0a876af17a1205c97eccd8dc2b58f
jmichalik.prismaticplanet@gmail.com <jmichalik.prismaticplanet@gmail.com>
2021-12-02 18:03:33 -0600

    AoC2021: Day 1 Part 2 init

--- 2021/Day 1/Day1_SonarSweep.r
+++ 2021/Day 1/Day1_SonarSweep.r
@@ -24,4 +24,39 @@ part1 <- function() {
     cat("  Depth increased ", sum(output == "Increased"), " times!")
 }
 
+part2 <- function() {
+    readings <- readLines("Day1_input.txt")
+
+    output <- character(length(readings))
+
+    last_reading <- get_three_reading_sum(readings, 1)
+
+    for (i in seq_len(length(readings))) {
+
+        # Ensure there are 3 numbers to sum
+        if (length(readings) - i >= 2) {
+            reading <- get_three_reading_sum(readings, i)
+
+            if (reading > last_reading) {
+                output[i] <- "Increased"
+            } else if (reading < last_reading) {
+                output[i] <- "Decreased"
+            } else {
+                output[i] <- "No Change"
+            }
+
+            last_reading <- reading
+        }
+    }
+
+    cat("  Depth increased ", sum(output == "Increased"), " times!")
+}
+
+get_three_reading_sum <- function (readings, start_index) {
+    sum(strtoi(readings[start_index]),
+        strtoi(readings[start_index + 1]),
+        strtoi(readings[start_index + 2]))
+}
+
 part1()
+part2()
