f5f6470228bb23ee222da8d768a86758dfaea111
jmichalik.prismaticplanet@gmail.com <jmichalik.prismaticplanet@gmail.com>
2023-12-06 21:54:55 -0600

    AoC2023: Day 3 Part 2

--- 2023/Day 03/Day3_GearRatios.r
+++ 2023/Day 03/Day3_GearRatios.r
@@ -4,6 +4,8 @@ get_day3_input_path <- function() {
     paste(getwd(), "/2023/Day 03/Day3_input.txt", sep = "")
 }
 
+gears <- c()
+
 part1 <- function() {
     lines <- readLines(get_day3_input_path())
 
@@ -53,7 +55,40 @@ part1 <- function() {
 }
 
 part2 <- function() {
-    print("TODO")
+    lines <- readLines(get_day3_input_path())
+
+    grid_length <- nchar(lines[1])
+    grid_height <- length(lines)
+
+    schematic <- t(matrix(unlist(strsplit(lines[1:grid_height], "")),
+                        ncol = grid_length))
+
+    for (i in seq_len(length(1:grid_height))) {
+        cur_num_parts <- c()
+        cur_num_coords <- list()
+
+        for (j in seq_len(length(1:grid_length))) {
+            cur_char <- schematic[i, j]
+
+            if (!is.na(as.numeric(cur_char))) {
+                cur_num_parts <- append(cur_num_parts, cur_char)
+                cur_num_coords[[length(cur_num_coords) + 1]] <- c(i, j)
+
+                if (j == grid_length && !is.na(cur_num_coords)) {
+                    part_num <- as.numeric(paste(cur_num_parts, collapse = ""))
+                    check_for_gears(cur_num_coords, schematic, grid_length, grid_height, part_num)
+                }
+            } else if (length(cur_num_coords) > 0) {
+                part_num <- as.numeric(paste(cur_num_parts, collapse = ""))
+                check_for_gears(cur_num_coords, schematic, grid_length, grid_height, part_num)
+
+                cur_num_parts <- c()
+                cur_num_coords <- c()
+            }
+        }
+    }
+
+    print(sum_gear_ratios())
 }
 
 is_part_num <- function(num_coords, schematic, length, height) {
@@ -70,6 +105,55 @@ is_part_num <- function(num_coords, schematic, length, height) {
     }
 
     is_part
+}
+
+check_for_gears <- function(num_coords, schematic, length, height, part_num) {
+    adj_coords <- get_adj_coords(num_coords, length, height)
+
+    for (c in seq_len(length(adj_coords))) {
+        coord <- adj_coords[[c]]
+        val <- schematic[coord[1], coord[2]]
+        if (val == "*") {
+            add_gear(coord, part_num)
+        }
+    }
+}
+
+add_gear <- function(coord, part_num) {
+    found_gear <- FALSE
+    for (g in seq_len(length(gears))) {
+        gear <- gears[[g]]
+
+        if (gear@row == coord[1] && gear@col == coord[2]) {
+            found_gear <- TRUE
+            gears[[g]]@adjparts <<- append(gear@adjparts, part_num)
+            break
+        }
+    }
+
+    if (!found_gear) {
+        gear <- new("Gear")
+
+        gear@row <- coord[1]
+        gear@col <- coord[2]
+        gear@adjparts <- c(part_num)
+
+        gears <<- append(gears, gear)
+    }
+}
+
+sum_gear_ratios <- function() {
+    ratios <- c()
+
+    for (g in seq_len(length(gears))) {
+        gear <- gears[[g]]
+        if (length(gear@adjparts) == 2) {
+            ratio <- gear@adjparts[1] * gear@adjparts[2]
+            ratios <- append(ratios, ratio)
+        }
+    }
+    
+    sum(ratios)
 }
 
 get_adj_coords <- function(coords, length, height) {
@@ -118,6 +202,12 @@ get_adj_coords <- function(coords, length, height) {
 
     adj_coords
 }
+
+setClass("Gear", representation(
+    row = "numeric",
+    col = "numeric",
+    adjparts = "vector")
+)
 
 part1()
 part2()
