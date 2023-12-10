f2baf573edbe440f68f5b06734e03e6c6717d610
jmichalik.prismaticplanet@gmail.com <jmichalik.prismaticplanet@gmail.com>
2023-12-10 10:18:57 -0600

    AoC2023: Day 8 Part 2 attempt

--- 2023/Day 08/Day8_HauntedWasteland.r
+++ 2023/Day 08/Day8_HauntedWasteland.r
@@ -46,8 +46,71 @@ part1 <- function() {
     print(num_steps)
 }
 
+# Brute force, too long to compute :(
 part2 <- function() {
-    print("TODO")
+    lines <- readLines(get_day8_input_path())
+
+    directions <- unlist(strsplit(lines[1], ""))
+    rooms <- list()
+
+    cur_rooms <- list()
+
+    for (i in 3:length(lines)) {
+        room_def <- lines[i]
+        room_split <- unlist(strsplit(room_def, "="))
+
+        cur_room <- trimws(room_split[1])
+
+        adj_rooms <- unlist(strsplit(trimws(room_split[2]), ","))
+        left_room <- gsub("\\(", "", adj_rooms[1])
+        right_room <- gsub("\\)", "", trimws(adj_rooms[2]))
+
+        rooms[[cur_room]] <- c(left_room, right_room)
+
+        if (length(grep("A$", cur_room)) > 0) {
+            cur_rooms[[cur_room]] <- rooms[[cur_room]]
+        }
+    }
+
+    num_steps <- 0
+    traversing <- TRUE
+
+    while (traversing) {
+        for (d in seq_len(length(directions))) {
+            new_rooms <- list()
+
+            for (i in seq_len(length(cur_rooms))) {
+                cur_room <- cur_rooms[[i]]
+                new_room <- cur_room[match(directions[d], dir_ind)]
+
+                new_rooms[[new_room]] <- rooms[[new_room]]
+            }
+
+            num_steps <- num_steps + 1
+            cur_rooms <- new_rooms
+
+            if (check_all_for_z(new_rooms)) {
+                traversing <- FALSE
+                break
+            }
+        }
+    }
+
+    print(num_steps)
+}
+
+check_all_for_z <- function(rooms) {
+    names <- names(rooms)
+    all_z <- TRUE
+
+    for (n in seq_len(length(names))) {
+        if (length(grep("Z$", names[n])) == 0) {
+            all_z <- FALSE
+            break
+        }
+    }
+
+    all_z
 }
 
 part1()
